1f37cff6c44209cf86ac7d4c7d6c4482096bfe92
Jorge Martins <77112497+jorginskn@users.noreply.github.com>
2023-01-31 19:26:19 -0300

    fit: criação dos metodos get e post para criar e buscar usuarios

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,35 @@
 import express from "express";
 
 const app = express();
+app.use(express.json())
+
+const users = ["Diego", "Jorge", "Luiz", "Murilo", "Cleiton", "Caio"];
 
 app.get("/users", (request, response) => {
-  console.log("Listagem de usuários");
+  const search = String(request.query.search);
+  const filteredUsers = search
+    ? users.filter((user) => user.includes(search))
+    : users;
 
-  response.json([
-    'Diego',
-    'Jorge',
-    'Luiz',
-    'Murilo',
-    'carai',
-    'Caio'
-  ])
+  console.log(search);
+  return response.json(filteredUsers);
+});
+
+app.get("/users/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const user = users[id];
+
+  return response.json(user);
+});
+
+app.post("/users", (request, response) => {
+  const data = request.body;
+  const user = {
+    name: data.name,
+    email: data.email,
+  };
+  console.log(data)
+  return response.json(user);
 });
 
 app.listen(3333);
