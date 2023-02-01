1adc4a380741e2c5f35d53d896b46cee9ea0c31c
Jorge Martins <77112497+jorginskn@users.noreply.github.com>
2023-02-01 12:28:45 -0300

    feat: criação de tabelas e  e do arquivo de database usando o sqlite

---
+++ src/database/migrations/01_create_items.ts
@@ -0,0 +1,11 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex) {
+    //criar a tabela
+    
+  }
+  
+  export async function down(knex: Knex) {
+     
+  }
+  
---
+++ src/database/migrations/02_create_point_items.ts
@@ -0,0 +1,7 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex) {
+  //criar a tabela
+}
+
+export async function down(knex: Knex) {}
