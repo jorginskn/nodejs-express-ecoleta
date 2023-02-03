a71a02113c1f73f953a9b508221220d6dc7e8d23
Jorge Martins <77112497+jorginskn@users.noreply.github.com>
2023-02-03 10:58:02 -0300

    fit: criação do acesso a tabela do db items,  mais criação dos campos de criar ponto de coleta

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,43 @@
-import express from "express";
+import express, { response } from "express";
+import knex from "./database/connection";
 
-const routes     = express.Router();
+const routes = express.Router();
 
-routes.get("/users", (request, response) => {
-  return response.json({ messsage: "Hello word" });
+routes.get("/items", async (request, response) => {
+  //SELECT * FROM items
+  const items = await knex("items").select("*");
+  const serializedItems = items.map((item) => {
+    return {
+      id: item.id,
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    };
+  });
+  return response.json({ serializedItems });
 });
 
+routes.post("points", async (request, response) => {
+  const  {
+    name,
+    email,
+    image,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items,
+
+  } =  request.body;  
+  await knex('points').insert({
+    name,
+    email,
+    image: 'image-fake',
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  })
+});
 export default routes;
